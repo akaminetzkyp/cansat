5abec7e2652d827359da0bda1c8b6521dd42b9d2
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2018-06-30 01:49:34 -0400

    Remove useless code and fix PEP8

--- bmp280.py
+++ bmp280.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#--------------------------------------
+# --------------------------------------
 #    ___  ___  _ ____
 #   / _ \/ _ \(_) __/__  __ __
 #  / , _/ ___/ /\ \/ _ \/ // /
@@ -17,145 +17,135 @@
 #
 # http://www.raspberrypi-spy.co.uk/
 #
-#--------------------------------------
+# --------------------------------------
 import smbus
 import time
 from ctypes import c_short
-from ctypes import c_byte
-from ctypes import c_ubyte
 
-DEVICE = 0x76 # Default device I2C address
+DEVICE = 0x76  # Default device I2C address
 
 
-bus = smbus.SMBus(1) # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1
-                     # Rev 1 Pi uses bus 0
+bus = smbus.SMBus(1)  # Rev 2 Pi, Pi 2 & Pi 3 uses bus 1, Rev 1 Pi uses bus 0
 
-def getShort(data, index):
-  # return two bytes from data as a signed 16-bit value
-  return c_short((data[index+1] << 8) + data[index]).value
 
-def getUShort(data, index):
-  # return two bytes from data as an unsigned 16-bit value
-  return (data[index+1] << 8) + data[index]
+def get_short(data, index):
+    # return two bytes from data as a signed 16-bit value
+    return c_short((data[index+1] << 8) + data[index]).value
 
-def getChar(data,index):
-  # return one byte from data as a signed char
-  result = data[index]
-  if result > 127:
-    result -= 256
-  return result
 
-def getUChar(data,index):
-  # return one byte from data as an unsigned char
-  result =  data[index] & 0xFF
-  return result
+def get_u_short(data, index):
+    # return two bytes from data as an unsigned 16-bit value
+    return (data[index+1] << 8) + data[index]
 
-def readBMP280ID(addr=DEVICE):
-  # Chip ID Register Address
-  REG_ID     = 0xD0
-  (chip_id, chip_version) = bus.read_i2c_block_data(addr, REG_ID, 2)
-  return (chip_id, chip_version)
 
-def readBMP280All(addr=DEVICE):
-  # Register Addresses
-  REG_DATA = 0xF7
-  REG_CONTROL = 0xF4
-  REG_CONFIG  = 0xF5
+def get_char(data, index):
+    # return one byte from data as a signed char
+    result = data[index]
+    if result > 127:
+        result -= 256
+    return result
 
-  REG_CONTROL_HUM = 0xF2
-  REG_HUM_MSB = 0xFD
-  REG_HUM_LSB = 0xFE
 
-  # Oversample setting - page 27
-  OVERSAMPLE_TEMP = 2
-  OVERSAMPLE_PRES = 2
-  MODE = 1
+def get_u_char(data, index):
+    # return one byte from data as an unsigned char
+    result = data[index] & 0xFF
+    return result
 
-  # Oversample setting for humidity register - page 26
-  OVERSAMPLE_HUM = 2
-  bus.write_byte_data(addr, REG_CONTROL_HUM, OVERSAMPLE_HUM)
 
-  control = OVERSAMPLE_TEMP<<5 | OVERSAMPLE_PRES<<2 | MODE
-  bus.write_byte_data(addr, REG_CONTROL, control)
+def read_id(addr=DEVICE):
+    # Chip ID Register Address
+    reg_id = 0xD0
+    (chip_id, chip_version) = bus.read_i2c_block_data(addr, reg_id, 2)
+    return chip_id, chip_version
 
-  # Read blocks of calibration data from EEPROM
-  # See Page 22 data sheet
-  cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
-  cal2 = bus.read_i2c_block_data(addr, 0xA1, 1)
-  cal3 = bus.read_i2c_block_data(addr, 0xE1, 7)
 
-  # Convert byte data to word values
-  dig_T1 = getUShort(cal1, 0)
-  dig_T2 = getShort(cal1, 2)
-  dig_T3 = getShort(cal1, 4)
+def read_data(addr=DEVICE):
+    # Register Addresses
+    reg_data = 0xF7
+    reg_control = 0xF4
 
-  dig_P1 = getUShort(cal1, 6)
-  dig_P2 = getShort(cal1, 8)
-  dig_P3 = getShort(cal1, 10)
-  dig_P4 = getShort(cal1, 12)
-  dig_P5 = getShort(cal1, 14)
-  dig_P6 = getShort(cal1, 16)
-  dig_P7 = getShort(cal1, 18)
-  dig_P8 = getShort(cal1, 20)
-  dig_P9 = getShort(cal1, 22)
+    reg_control_hum = 0xF2
 
-  dig_H1 = getUChar(cal2, 0)
-  dig_H2 = getShort(cal3, 0)
-  dig_H3 = getUChar(cal3, 2)
+    # Oversample setting - page 27
+    oversample_temp = 2
+    oversample_pres = 2
+    mode = 1
 
-  dig_H4 = getChar(cal3, 3)
-  dig_H4 = (dig_H4 << 24) >> 20
-  dig_H4 = dig_H4 | (getChar(cal3, 4) & 0x0F)
+    # Oversample setting for humidity register - page 26
+    oversample_hum = 2
+    bus.write_byte_data(addr, reg_control_hum, oversample_hum)
 
-  dig_H5 = getChar(cal3, 5)
-  dig_H5 = (dig_H5 << 24) >> 20
-  dig_H5 = dig_H5 | (getUChar(cal3, 4) >> 4 & 0x0F)
+    control = oversample_temp << 5 | oversample_pres << 2 | mode
+    bus.write_byte_data(addr, reg_control, control)
+  
+    # Read blocks of calibration data from EEPROM
+    # See Page 22 data sheet
+    cal1 = bus.read_i2c_block_data(addr, 0x88, 24)
+  
+    # Convert byte data to word values
+    dig_t1 = get_u_short(cal1, 0)
+    dig_t2 = get_short(cal1, 2)
+    dig_t3 = get_short(cal1, 4)
+  
+    dig_p1 = get_u_short(cal1, 6)
+    dig_p2 = get_short(cal1, 8)
+    dig_p3 = get_short(cal1, 10)
+    dig_p4 = get_short(cal1, 12)
+    dig_p5 = get_short(cal1, 14)
+    dig_p6 = get_short(cal1, 16)
+    dig_p7 = get_short(cal1, 18)
+    dig_p8 = get_short(cal1, 20)
+    dig_p9 = get_short(cal1, 22)
 
-  dig_H6 = getChar(cal3, 6)
+    # Wait in ms (Datasheet Appendix B:
+    # Measurement time and current calculation)
+    wait_time = 1.25 + (2.3 * oversample_temp) + ((2.3 * oversample_pres) +
+                                                  0.575) + ((2.3
+                                                            * oversample_hum)
+                                                            + 0.575)
+    time.sleep(wait_time/1000)  # Wait the required time  
+  
+    # Read temperature/pressure/humidity
+    data = bus.read_i2c_block_data(addr, reg_data, 8)
+    pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+    temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+  
+    # Refine temperature
+    var1 = ((temp_raw >> 3 - dig_t1 << 1) * dig_t2) >> 11
+    var2 = ((((temp_raw >> 4 - dig_t1) * (temp_raw >> 4 - dig_t1)) >> 12)
+            * dig_t3) >> 14
+    t_fine = var1 + var2
+    temperature = float(((t_fine * 5) + 128) >> 8)
+  
+    # Refine pressure and adjust for temperature
+    var1 = t_fine / 2.0 - 64000.0
+    var2 = var1 * var1 * dig_p6 / 32768.0
+    var2 = var2 + var1 * dig_p5 * 2.0
+    var2 = var2 / 4.0 + dig_p4 * 65536.0
+    var1 = (dig_p3 * var1 * var1 / 524288.0 + dig_p2 * var1) / 524288.0
+    var1 = (1.0 + var1 / 32768.0) * dig_p1
+    if var1 == 0:
+        pressure = 0
+    else:
+        pressure = 1048576.0 - pres_raw
+        pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
+        var1 = dig_p9 * pressure * pressure / 2147483648.0
+        var2 = pressure * dig_p8 / 32768.0
+        pressure = pressure + (var1 + var2 + dig_p7) / 16.0
+  
+    return temperature/100.0, pressure/100.0
 
-  # Wait in ms (Datasheet Appendix B: Measurement time and current calculation)
-  wait_time = 1.25 + (2.3 * OVERSAMPLE_TEMP) + ((2.3 * OVERSAMPLE_PRES) + 0.575) + ((2.3 * OVERSAMPLE_HUM)+0.575)
-  time.sleep(wait_time/1000)  # Wait the required time  
-
-  # Read temperature/pressure/humidity
-  data = bus.read_i2c_block_data(addr, REG_DATA, 8)
-  pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
-  temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-
-  #Refine temperature
-  var1 = ((((temp_raw>>3)-(dig_T1<<1)))*(dig_T2)) >> 11
-  var2 = (((((temp_raw>>4) - (dig_T1)) * ((temp_raw>>4) - (dig_T1))) >> 12) * (dig_T3)) >> 14
-  t_fine = var1+var2
-  temperature = float(((t_fine * 5) + 128) >> 8);
-
-  # Refine pressure and adjust for temperature
-  var1 = t_fine / 2.0 - 64000.0
-  var2 = var1 * var1 * dig_P6 / 32768.0
-  var2 = var2 + var1 * dig_P5 * 2.0
-  var2 = var2 / 4.0 + dig_P4 * 65536.0
-  var1 = (dig_P3 * var1 * var1 / 524288.0 + dig_P2 * var1) / 524288.0
-  var1 = (1.0 + var1 / 32768.0) * dig_P1
-  if var1 == 0:
-    pressure=0
-  else:
-    pressure = 1048576.0 - pres_raw
-    pressure = ((pressure - var2 / 4096.0) * 6250.0) / var1
-    var1 = dig_P9 * pressure * pressure / 2147483648.0
-    var2 = pressure * dig_P8 / 32768.0
-    pressure = pressure + (var1 + var2 + dig_P7) / 16.0
-
-  return temperature/100.0, pressure/100.0
 
 def main():
+    (chip_id, chip_version) = read_id()
+    print("Chip ID:", chip_id)
+    print("Version:", chip_version)
 
-  (chip_id, chip_version) = readBMP280ID()
-  print("Chip ID     :", chip_id)
-  print("Version     :", chip_version)
+    temperature, pressure = read_data()
 
-  temperature,pressure = readBMP280All()
+    print("Temperature: ", temperature, "ºC")
+    print("Pressure: ", pressure, "hPa")
 
-  print("Temperature : ", temperature, "C")
-  print("Pressure : ", pressure, "hPa")
 
-if __name__=="__main__":
-   main()
+if __name__ == "__main__":
+    main()
